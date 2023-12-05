c39c85f749c46d48ee37ef77343d636a6a9eb173
Charicauth <50031787+charicauth@users.noreply.github.com>
2023-12-05 23:52:58 +0100

    loader update (removing js)

--- js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(() => {
-        document.getElementById("loader").style.display = "none";
-        document.getElementById("content").style.display = "block";
-    }, 2000); 
-});
